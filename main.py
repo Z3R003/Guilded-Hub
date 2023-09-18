84c92c63a1b6191f1f7a8d836ba0e9e47e768349
Z3ROOE <139226148+Z3R003@users.noreply.github.com>
2023-09-18 20:04:37 +0200

    main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def fake_account_generator_title():
-    global total, generated, failed, joined, checked,status_num,presence
+    global total, generated, failed, joined, checked
     ctypes.windll.kernel32.SetConsoleTitleW(f"『 Guilded Hub 』 By ~Z3R003~ | Fake Account Generator | Created : {generated} ~ Joined : {joined}")
 def real_account_generator_title():
     global total, generated, failed, joined, checked
@@ -64,7 +64,7 @@ def chat_spammer_title():
     ctypes.windll.kernel32.SetConsoleTitleW(f"『 Guilded Hub 』 By ~Z3R003~ | Chat Spammer | Messages Sent : {messages}")
 def account_status_presence_title():
     global total, generated, failed, joined, checked, messages,status_num,presence
-    ctypes.windll.kernel32.SetConsoleTitleW(f"『 Guilded Hub 』 By ~Z3R003~ | Account-Status-Presence | Status : {status_num} ~ Presence : {presence}")
+    ctypes.windll.kernel32.SetConsoleTitleW(f"『 Guilded Hub 』 By ~Z3R003~ | Account-Status-Presence | Status : {status_num} ~ Online : {presence}")
 
 def email_gen():
     email = ''.join(random.choices(string.ascii_letters + string.digits, k =10))
@@ -536,7 +536,7 @@ def account_status_presence(email,password):
         if set_status.status_code == 200:
             with output_lock:
                 time_rn = get_time()
-                print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({green}+{gray}) {lightcyan} Online Set {gray} | ", end="")
+                print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({green}+{gray}) {lightcyan} Status Set {gray} | ", end="")
                 sys.stdout.flush()
                 Write.Print(f"{email} : {password}\n", Colors.yellow_to_red, interval=0.000)  
                 status_num += 1
@@ -550,7 +550,7 @@ def account_status_presence(email,password):
         if set_presence.status_code == 200:
             with output_lock:
                 time_rn = get_time()
-                print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({green}+{gray}) {lightcyan} Status Set {gray} | ", end="")
+                print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({green}+{gray}) {lightcyan} Online Set {gray} | ", end="")
                 sys.stdout.flush()
                 Write.Print(f"{email} : {password}\n", Colors.yellow_to_red, interval=0.000)  
                 presence += 1
